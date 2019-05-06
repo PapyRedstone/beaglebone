387f5d441a912175a36cd20637b1fc4bb240017a
Alex <febrealex@yahoo.fr>
2019-05-06 10:06:11 +0200

    e

--- Leds_analogique/src/main.c
+++ Leds_analogique/src/main.c
@@ -5,9 +5,9 @@
 int main(void){
   while(1){
     int val = read_ADC();
-    if(val < 1800*2/3){
+    if(val < 1800*1/3){
       control_RVB(1,0,0);
-    }else if(val < 1800*1/3){
+    }else if(val < 1800*2/3){
       control_RVB(0,1,0);
     }else{
       control_RVB(0,0,1);
