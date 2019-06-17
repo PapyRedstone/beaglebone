5f67acc383a7be5a8f629c15b8a0963a75813e39
DrWoodMan <adrien.lebourgeois@protonmail.com>
2019-06-17 11:29:20 +0200

    1

--- LedPWM_Thread/src/main.c
+++ LedPWM_Thread/src/main.c
@@ -2,7 +2,7 @@
 #define bool int
 
 int main(void){
-  FILE *file = fopen("/sys/devices/ocp.3/pwm_test_P9_14.15/run", "w");
+  FILE *file = fopen("/sys/devices/ocp.3/pwm_test_P9_14.15/run", "w");// activer PWM sur le hard
   fprintf(file, "%d", 1);
   fflush(file);
   fclose(file);
@@ -14,6 +14,6 @@ int main(void){
   pthread_create(&tBlue, NULL,  blue,  NULL);
 
   pthread_join(tRed,   NULL);
-  pthread_join(tGreen, NULL);
+  pthread_join(tGreen, NULL);//attend que les threads se finissent
   pthread_join(tBlue,  NULL);
 }
