c5688ae49a4f35b5eed7f0bf64630f210cf87bf3
DrWoodMan <adrien.lebourgeois@protonmail.com>
2019-06-17 14:09:37 +0200

    2

--- Projet/include/functions.h
+++ Projet/include/functions.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void selection();
 void commande_radio(char tube, char *etat);
--- Projet/src/functions.c
+++ Projet/src/functions.c
@@ -1,6 +1,36 @@
 #include "functions.h"
 
-void selection(){}
+void setLEDetat(char x, char y, char value){
+  char fileName[29];
+  sprintf(fileName, "/sys/class/gpio/gpio%d/value", x*32+y);
+  FILE *file = fopen(fileName, "w");
+  fprintf(file, "%d", value);
+  fflush(file);
+  fclose(file);
+}
+
+void control_RVB(char R, char V, char B){
+  setLEDetat(1,18, R);
+  setLEDetat(1,28, V);
+  setLEDetat(0, 3, B);
+}
+
+int read_ADC(){
+  unsigned int etat;
+  FILE *file = fopen("/sys/devices/ocp.3/helper.14/AIN3", "r");
+  fscanf(file, "%d", &etat);
+  fclose(file);
+  return etat;
+}
+
+unsigned int etatPB(){
+  FILE *file = NULL;
+  unsigned int etat;
+  file = fopen("/sys/class/gpio/gpio48/value", "r");
+  fscanf(file, "%d", &etat);
+  fclose(file);
+  return etat;
+}
 
 void commande_radio(char tube, char* etat){
   switch(tube){
@@ -28,5 +58,28 @@ void commande_radio(char tube, char* etat){
   default:
     printf("ERROR: Wrong tube type received\n");
     break;
+  }
+}
+
+void selection(){
+  static char* etatRVB = malloc(sizeof(char)*3);
+  char choixRVB;
+  int adc = read_ADC();
+  
+  if(adc < 1800*1/3){
+    control_RVB(1,0,0);
+    choixRVB = 'R';
+  }else if(adc < 1800*2/3){
+    control_RVB(0,1,0);
+    choixRVB = 'v';
+  }else{
+    control_RVB(0,0,1);
+    choixRVB = 'B';
+  }
+
+  if(!etatPB()){
+    commande_radio(choixRVB, etatRVB);
+  }else{
+    eteintLED();
   }
 }
